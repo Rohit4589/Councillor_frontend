d3342066529adc5d908ac26eef494adf380efd2d
Om Kolekar <omkolekar2712@gmail.com>
2025-12-18 15:19:51 +0530

    Officer Final UI change

--- councillor_app_Frontend/src/Screens/Officers.jsx
+++ councillor_app_Frontend/src/Screens/Officers.jsx
@@ -3,10 +3,7 @@ import "../Style/officers.css";
 export default function Officers() {
   const officers = [
     { name: "Rajesh Sharma", phone: "+91 9876543210" },
-    { name: "Amit Verma", phone: "+91 9123456780" },
-    { name: "Sneha Patil", phone: "+91 9988776655" },
-    { name: "Rohit Kulkarni", phone: "+91 9090909090" },
-    { name: "Neha Joshi", phone: "+91 8899776655" },
+    
   ];
 
   return (
